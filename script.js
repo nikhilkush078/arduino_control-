b4d53ced30267dd8ca37bcf26106177961ff5ebd
nikhilkush078 <nikhilkushwah078@gmail.com>
2025-01-15 16:40:09 +0530

    script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let writer;
 
 async function connectToArduino() {
   try {
-    port = await navigator.serial.requestPort(); // Request a port
+    port = await navigator.serial.requestPort(); // Request a port when user clicks the button
     await port.open({ baudRate: 9600 }); // Open the port
     writer = port.writable.getWriter(); // Get the writer
     alert("Connected to Arduino!");
@@ -18,12 +18,9 @@ async function sendSignal(signal) {
     return;
   }
   try {
-    await writer.write(new TextEncoder().encode(signal));
+    await writer.write(new TextEncoder().encode(signal)); // Send the signal
     console.log("Signal sent: " + signal);
   } catch (error) {
     alert("Failed to send signal: " + error.message);
   }
 }
-
-// Automatically prompt the user to connect on page load
-connectToArduino();
